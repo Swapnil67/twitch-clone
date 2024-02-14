52b772050d76347323bbfc354f4d394bb37e4d08
swapniladsul017@gmail.com <swapniladsul017@gmail.com>
2024-02-15 04:26:08 +0530

    Follow & Unfollow service

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -4,7 +4,7 @@ import { Wrapper } from './wrapper'
 import { getRecommended } from '@/lib/recommended-service'
 import { getFollowedUsers } from '@/lib/follow-service'
 
-import Following from './following'
+import Following, { FollowingSkeleton } from './following'
 import Toggle, { ToggleSkeleton } from './toggle'
 import Recommended, { RecommendedSkeleton } from './recommended'
 
@@ -17,8 +17,8 @@ const Sidebar = async () => {
     <Wrapper>
       <Toggle />
       <div className="space-y-4 pt-4 lg:pt-0">
-        <Recommended data={recommended} />
         <Following data={following} />
+        <Recommended data={recommended} />
       </div>
     </Wrapper>
   )
@@ -28,6 +28,7 @@ export const SidebarSkeleton = () => {
   return (
     <aside className='fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50'>
       <ToggleSkeleton />
+      <FollowingSkeleton />
       <RecommendedSkeleton />
     </aside>
   )
--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -4,6 +4,7 @@ import { useSidebar } from "@/store/use-sidebar";
 import { useEffect, useState } from "react";
 import { ToggleSkeleton } from "./toggle";
 import { RecommendedSkeleton } from "./recommended";
+import { FollowingSkeleton } from "./following";
 
 interface WrapperProps {
   children: React.ReactNode;
@@ -29,6 +30,7 @@ export const Wrapper = ({ children }: WrapperProps) => {
         )}
       >
         <ToggleSkeleton />
+        <FollowingSkeleton />
         <RecommendedSkeleton />
       </aside>
     );
